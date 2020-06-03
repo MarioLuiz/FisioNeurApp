48480ddcb1ba0586232a4271a16852901cf6fc9c
unknown <mario.arruda@nastek.com.br>
2020-06-03 17:20:23 -0400

    Model frases e mock criados.

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,15 @@
+import { Frase } from './../shared/model/frase.model';
+
+const FRASES : Frase [] = [
+    {fraseEng:'I like to leart' , frasePtBr: 'Eu gosto de aprender'},
+    {fraseEng:'How are you?' , frasePtBr: 'Como vai você?'},
+    {fraseEng:'I watch tv' , frasePtBr: 'Eu assisto tv'},
+    {fraseEng:'I like apple' , frasePtBr: 'Eu gosto de maça'},
+    {fraseEng:'I watch sports' , frasePtBr: 'Eu assisto esportes'},
+    {fraseEng:'I like pineapple' , frasePtBr: 'Eu gosto de abacaxi'},
+    {fraseEng:'Today is a beautiful day to stroll in the park' , frasePtBr: 'Hoje está um belo dia para passear no parque'},
+    {fraseEng:'It looks like it will rain today' , frasePtBr: 'Parece que irá chover hoje'},
+    {fraseEng:'Does this dress make me fat?' , frasePtBr: 'Este vestido me deixa gorda?'},
+    {fraseEng:'I love to draw dogs' , frasePtBr: 'Eu amo desenhar cachorros'},
+    {fraseEng:'How are you doing?' , frasePtBr: 'How are you doing?'}
+];
