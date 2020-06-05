5613640f7a7a74e2425ec08b4ff7dca461150195
unknown <mario.arruda@nastek.com.br>
2020-06-05 18:10:55 -0400

    Adiconada logica de vitória e derrota.

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 
 import { Frase } from './../shared/model/frase.model';
 import { FRASES } from './frases-mock';
@@ -8,20 +8,30 @@ import { FRASES } from './frases-mock';
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
-export class PainelComponent implements OnInit {
+export class PainelComponent implements OnInit, OnDestroy {
 
   public frases: Frase[] = FRASES;
   public instrucao: string = 'Traduza a frase:';
   public resposta: string = '';
+
   public rodada: number = 0;
   public rodadaFrase: Frase;
+
   public progresso: number = 0;
+
+  public tentativas: number = 4;
+
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter();
 
   constructor() {
     this.atualizaRodada();
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    //console.log('Componente painel foi destruido');
   }
 
   public atualizaResposta(evento: Event): void {
@@ -32,7 +42,8 @@ export class PainelComponent implements OnInit {
   public verificarResposta(): void {
 
     if (this.rodadaFrase.frasePtBr == this.resposta) {
-      alert('A tradução está correta');
+      //alert('A tradução está correta');
+
       //trocar pergunta da rodada
       this.rodada++;
 
@@ -40,13 +51,22 @@ export class PainelComponent implements OnInit {
       this.progresso = this.progresso + (100 / this.frases.length);
       //console.log('this.progresso: ', this.progresso);
 
+      if (this.rodada === 10) {
+        alert('Concluiu as traduções com sucesso!')
+        this.encerrarJogo.emit('vitoria');
+      }
+
       //atualizando o obj da rodada
       this.atualizaRodada();
 
     } else {
-      alert('A tradução está errada');
+      //alert('A tradução está errada');
+      this.tentativas--;
+      if (this.tentativas === -1) {
+        alert('Você perdeu todas as tentativas');
+        this.encerrarJogo.emit('derrota');
+      }
     }
-
   }
 
 
