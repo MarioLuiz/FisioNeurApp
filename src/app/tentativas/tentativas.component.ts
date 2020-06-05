601d45abb4233598a62aa3b284fc7e461798852c
unknown <mario.arruda@nastek.com.br>
2020-06-05 18:11:21 -0400

    Adicionada logica coração e perda do mesmo.

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,18 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+
+import { Coracao } from './../shared/model/coracao.model';
 
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
 
-  public coracaoVazio: string = '/assets/coracao_vazio.png';
-  public coracaoCheio: string = '/assets/coracao_cheio.png';
-  
-  constructor() { }
+  @Input() public tentativas: number;
+
+  public coracoes: Coracao[] = [
+    new Coracao(true), new Coracao(true), new Coracao(true), new Coracao(true)
+  ];
+
+  constructor() {
+    //console.log('this.coracoes', this.coracoes);
+  }
+
+  ngOnChanges() {
+    //this.tentativas
+    //this.coracoes.length
+    if (this.tentativas !== this.coracoes.length) {
+      let indice = this.coracoes.length - this.tentativas;
+      this.coracoes[indice - 1].cheio = false;
+    }
+    console.log('Tentativas recebidas do painel', this.tentativas);
+  }
 
   ngOnInit() {
+
   }
 
 }
