2697d60bba16d9cd1b288be1517a9facd79db43e
Mario Luiz <marioluiz.arruda@gmail.com>
2020-06-04 22:09:28 -0400

    Adicionada lógica na barra de progresso.

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -10,22 +10,45 @@ import { FRASES } from './frases-mock';
 })
 export class PainelComponent implements OnInit {
 
-  public frases : Frase[] = FRASES;
+  public frases: Frase[] = FRASES;
   public instrucao: string = 'Traduza a frase:';
   public resposta: string;
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
 
-  constructor() { console.log('this.frases: ', this.frases)}
+  public progresso: number = 0;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log('this.rodadaFrase: ', this.rodadaFrase);
+  }
 
   ngOnInit() {
   }
 
   public atualizaResposta(evento: Event): void {
-    this.resposta = (<HTMLInputElement>evento.target).value ;
+    this.resposta = (<HTMLInputElement>evento.target).value;
     //console.log('this.resposta: ',this.resposta);
   }
 
   public verificarResposta(): void {
-    console.log('verificarResposta: ', this.resposta);
+
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('A tradução está correta');
+      //trocar pergunta da rodada
+      this.rodada++;
+
+      //progresso
+      this.progresso = this.progresso + (100 / this.frases.length) ;
+      console.log('this.progresso: ',this.progresso);
+
+      //atualizando o obj da rodada
+      this.rodadaFrase = this.frases[this.rodada];
+
+    } else {
+      alert('A tradução está errada');
+    }
+
   }
 
 }
